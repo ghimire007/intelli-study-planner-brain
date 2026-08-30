d43856e9cac1b6d0461215772ac50cf3eea67abc
Gia Han <giahanmailhere@gmail.com>
2026-08-30 19:13:09 +1000

    add import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
 
