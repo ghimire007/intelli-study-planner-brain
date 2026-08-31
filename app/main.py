8a7e6c5738eaa00dca9edcbcf55110e49167cbfc
Gia Han <giahanmailhere@gmail.com>
2026-08-31 11:04:44 +1000

    remove unused import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 from pathlib import Path
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
 
